492f02a1207c52101547ada5af7af4cc648d1f4b
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-10-20 16:56:31 -0500

    tutorials page 1 and 2
    plus generated the hero-detail component at the start of page 3

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Abbey' },
+  { id: 12, name: 'Man' },
+  { id: 13, name: 'Zeus' },
+  { id: 14, name: 'Dog Ball' },
+  { id: 15, name: 'Mighty Wolf' },
+]
