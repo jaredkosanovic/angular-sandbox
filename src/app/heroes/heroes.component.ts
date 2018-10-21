3a77af6c78fe2a12d437c4963ab59c393f31ad53
Jared Kosanovic <kosanovj@oregonstate.edu>
2018-10-20 21:09:38 -0500

    end of tutorial page 5
    implemented dashboard, message service, and app routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,13 +10,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
